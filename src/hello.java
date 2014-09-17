ae781f47fdccefed9c7fd48267b23e6aa13a06d5
Johnlee Alvarez <johnlee.alvarez@colorado.edu>
2014-09-17 12:25:33 -0600

    Update hello.java

--- src/hello.java
+++ src/hello.java
@@ -2,6 +2,7 @@ public class hello {
 	public static void main(String[] args){
 		System.out.println("hello world");
 		System.out.println("hello part 2");
+		System.out.println("addition for TA");
 	}
 
 }
